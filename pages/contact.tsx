089e1b9ecb36b8a599f97d912a4184c7ca07c2b4
iamhk12 <hkothari247@gmail.com>
2023-12-30 19:36:24 +0530

    Changes : Font size - logo

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -9,7 +9,7 @@ export default function Contact() {
         <div className="md:p-10 p-4 w-full h-full fixed">
           <div className="border-[1px] shadow-slate-200  border-white w-full h-full rounded-none overflow-y-auto overflow-x-hidden  flex flex-col items-center relative pb-56">
             <nav className="flex justify-between p-4 md:flex-row flex-col md:w-[80%] w-[90%] mb-12 mt-4">
-              <h1 className="md:text-2xl md:text-[1.5rem] text-xl mb-5 font-medium logoCss text-center">R S FURNITURE</h1>
+            <h1 className="md:text-2xl md:text-[1.5rem] text-3xl mb-5 font-medium logoCss text-center">R S FURNITURE</h1>
               <div className="flex flow-row md:justify-evenly justify-between md:w-5/12 w-full md:text-xl text-base"> 
                 <button onClick={()=> router.push('/')}>Home</button> | 
                 <button onClick={()=> router.push('/services')}>Services</button> |
