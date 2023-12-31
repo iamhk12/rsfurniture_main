2bec60ad3a5b5d14f0d4c61d4c876236e26f9e90
iamhk12 <hkothari247@gmail.com>
2024-01-01 00:32:10 +0530

    Changes : Services Page content added, etc.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,110 +1,179 @@
 "use client";
-import Image from "next/image"
-import heroImage from "@/public/heroimage.webp"
-import card1Image from "@/public/card1.webp"
-import card2Image from "@/public/card2.webp"
-import card3Image from "@/public/card3.webp"
+import Image from "next/image";
+import heroImage from "@/public/heroimage.webp";
+import card1Image from "@/public/card1.webp";
+import card2Image from "@/public/card2.webp";
+import card3Image from "@/public/card3.webp";
 import { AiOutlineHome, AiOutlineArrowRight } from "react-icons/ai";
-import { useRouter } from "next/router"
+import { useRouter } from "next/router";
 import { useEffect, useRef } from "react";
 import { IoIosArrowUp } from "react-icons/io";
 
 export default function Home() {
-  const router = useRouter()
+  const router = useRouter();
   const scrollContainerRef = useRef<HTMLElement>(null);
 
-  useEffect(()=>{
-    document.title = "R. S. Furniture"
-  })
+  useEffect(() => {
+    document.title = "R. S. Furniture";
+  });
 
   const scrollToTop = () => {
     if (scrollContainerRef.current) {
-      scrollContainerRef.current.scrollIntoView({ behavior: 'smooth' });
+      scrollContainerRef.current.scrollIntoView({ behavior: "smooth" });
     }
   };
   return (
     <>
       <div className="h-full w-full absolute ">
         <div className="md:p-10 p-4 w-full h-full fixed">
-          <div className="border-[1px] shadow-slate-200  border-white w-full h-full rounded-none overflow-y-auto overflow-x-hidden  flex flex-col items-center relative" id="ScrollContainer">
-            <span ref={scrollContainerRef} ></span>
-            <nav className="flex justify-between p-4 md:flex-row flex-col md:w-[80%] w-[90%] mb-12 mt-4" >
-              <h1 className="md:text-2xl md:text-[1.5rem] text-3xl mb-5 font-medium logoCss text-center border-y-2 border-y-gray-600 py-1">R S FURNITURE</h1>
-              <div className="flex flow-row md:justify-evenly justify-between items-center md:w-5/12 w-full md:text-xl text-base"> 
-                <button className="bn9"><span> Home</span></button> | 
-                <button onClick={()=> router.push('/services')}> Services</button> | 
-                <button onClick={()=> router.push('/contact')}> Contact Us</button> 
+          <div
+            className="border-[1px] shadow-slate-200  border-white w-full h-full rounded-none overflow-y-auto overflow-x-hidden  flex flex-col items-center relative"
+            id="ScrollContainer"
+          >
+            <span ref={scrollContainerRef}></span>
+            <nav className="flex justify-between p-4 md:flex-row flex-col md:w-[80%] w-[90%] mb-12 mt-4">
+              <h1 className="md:text-2xl md:text-[1.5rem] text-3xl mb-5 font-medium logoCss text-center border-y-2 border-y-gray-600 py-1">
+                R S FURNITURE
+              </h1>
+              <div className="flex flow-row md:justify-evenly justify-between items-center md:w-5/12 w-full md:text-xl text-base">
+                <button className="bn9">
+                  <span> Home</span>
+                </button>{" "}
+                |
+                <button onClick={() => router.push("/services")}>
+                  {" "}
+                  Services
+                </button>{" "}
+                |
+                <button onClick={() => router.push("/contact")}>
+                  {" "}
+                  Contact Us
+                </button>
               </div>
             </nav>
             {/* HERO SECTION  */}
-            <section className="w-[90%] p-5 flex md:flex-row flex-col items-center justify-evenly mb-14"> 
-              <Image src={heroImage} alt="Hero" className="md:w-[40%] w-[100%] rounded-xl mb-7"/>
+            <section className="w-[90%] p-5 flex md:flex-row flex-col items-center justify-evenly mb-14">
+              <Image
+                src={heroImage}
+                alt="Hero"
+                className="md:w-[40%] w-[100%] rounded-xl mb-7"
+              />
               <div className="md:w-[50%] w-[90%] text-right flex flex-col md:gap-10 justify-center ">
-                
                 <div className="flex flex-col items-end md:text-3xl md:gap-6 text-2xl">
-                  <AiOutlineHome/>
+                  <AiOutlineHome />
                   <h1 className=" text-['Baloo Bhai 2'] ">
                     Discover Timeless Elegance
                   </h1>
                 </div>
                 <h1 className="md:text-[75px] text-4xl font-['Sevillana']">
-                    in Every Piece
+                  in Every Piece
                 </h1>
               </div>
             </section>
-            
-            
-            <button className="border-2 max-w-[90%] text-base border-white my-10 rounded-full p-5 font-medium px-10 hover:bg-white hover:text-black transition-all hover:shadow-[10px_0px_120px_rgba(25,171,255,0.8)]"> 
-              <span className="flex flex-row items-center gap-4"><span className="text-left">Check out the services we provide</span> <span><AiOutlineArrowRight/></span> </span>
+
+            <button className="border-2 max-w-[90%] text-base border-white my-10 rounded-full p-5 font-medium px-10 hover:bg-white hover:text-black transition-all hover:shadow-[10px_0px_120px_rgba(25,171,255,0.8)]">
+              <span className="flex flex-row items-center gap-4">
+                <span className="text-left">
+                  Check out the services we provide
+                </span>{" "}
+                <span>
+                  <AiOutlineArrowRight />
+                </span>{" "}
+              </span>
             </button>
 
             {/* EXPERIENCE SECTION  */}
-            <h1 className="font-[Poppins] text-left md:w-[85%] w-[90%] md:text-[40px] text-2xl mb-14"> EXPERIENCE . . . </h1>
-            <section className="w-[100%] p-5 flex md:flex-row flex-col items-center justify-evenly"> 
-              
+            <h1 className="font-[Poppins] text-left md:w-[85%] w-[90%] md:text-[40px] text-2xl mb-14">
+              {" "}
+              EXPERIENCE . . .{" "}
+            </h1>
+            <section className="w-[100%] p-5 flex md:flex-row flex-col items-center justify-evenly">
               <div className="card bg-white shadow-md p-4 rounded-lg mb-4 md:w-[27%] w-full flex flex-col hover:scale-110 transition-all">
                 <div className="relative w-[100%] h-64 mb-4 rounded-lg overflow-hidden">
-                  <Image src={card1Image} alt="Card Image" layout="fill" objectFit="cover" />
+                  <Image
+                    src={card1Image}
+                    alt="Card Image"
+                    layout="fill"
+                    objectFit="cover"
+                  />
                 </div>
                 {/* <div className="max-w-44"> */}
-                  <h2 className="text-xl font-semibold mb-2 text-black">Comfortable Designs</h2>
-                  <p className="text-gray-600">Explore our collection of stylish furniture that complements your lifestyle.</p>
+                <h2 className="text-xl font-semibold mb-2 text-black">
+                  Comfortable Designs
+                </h2>
+                <p className="text-gray-600">
+                  Explore our collection of stylish furniture that complements
+                  your lifestyle.
+                </p>
                 {/* </div> */}
               </div>
               <div className="card bg-white shadow-md p-4 rounded-lg mb-4 md:w-[27%] w-full flex flex-col hover:scale-110 transition-all">
                 <div className="relative w-[100%] h-64 mb-4 rounded-lg overflow-hidden">
-                  <Image src={card2Image} alt="Card Image" layout="fill" objectFit="cover" />
+                  <Image
+                    src={card2Image}
+                    alt="Card Image"
+                    layout="fill"
+                    objectFit="cover"
+                  />
                 </div>
                 {/* <div className="max-w-44"> */}
-                  <h2 className="text-xl font-semibold mb-2 text-black"> Affordable Solutions</h2>
-                  <p className="text-gray-600">Find affordable and quality furniture solutions for every room in your home.</p>
+                <h2 className="text-xl font-semibold mb-2 text-black">
+                  {" "}
+                  Affordable Solutions
+                </h2>
+                <p className="text-gray-600">
+                  Find affordable and quality furniture solutions for every room
+                  in your home.
+                </p>
                 {/* </div> */}
               </div>
               <div className="card bg-white shadow-md p-4 rounded-lg mb-4 md:w-[27%] w-full flex flex-col hover:scale-110 transition-all">
                 <div className="relative w-[100%] h-64 mb-4 rounded-lg overflow-hidden">
-                  <Image src={card3Image} alt="Card Image" layout="fill" objectFit="cover" />
+                  <Image
+                    src={card3Image}
+                    alt="Card Image"
+                    layout="fill"
+                    objectFit="cover"
+                  />
                 </div>
                 {/* <div className="max-w-44"> */}
-                  <h2 className="text-xl font-semibold mb-2 text-black"> Comfortable Designs</h2>
-                  <p className="text-gray-600">Experience comfort with our thoughtfully designed furniture pieces.</p>
+                <h2 className="text-xl font-semibold mb-2 text-black">
+                  {" "}
+                  Comfortable Designs
+                </h2>
+                <p className="text-gray-600">
+                  Experience comfort with our thoughtfully designed furniture
+                  pieces.
+                </p>
                 {/* </div> */}
               </div>
             </section>
 
-
             {/*ABOUT Section*/}
-            <h1 className="font-[Poppins] text-center w-[90%] md:text-[43px]  font-bold text-3xl my-14">ABOUT US </h1>
-            <section className="w-full p-5 flex md:flex-row flex-col items-center justify-evenly text-center"> 
+            <h1 className="font-[Poppins] text-center w-[90%] md:text-[43px]  font-bold text-3xl my-14">
+              ABOUT US{" "}
+            </h1>
+            <section className="w-full p-5 flex md:flex-row flex-col items-center justify-evenly text-center">
               <div className="md:w-[30%] w-[90%] text-white mb-10">
-                <h2 className="md:text-4xl text-2xl md:font-bold font-semibold mb-3 text-rose-300 leading-tight">Stories in Wood</h2>
+                <h2 className="md:text-4xl text-2xl md:font-bold font-semibold mb-3 text-rose-300 leading-tight">
+                  Stories in Wood
+                </h2>
                 <p className="text-gray-400 leading-loose">
-                  Welcome to R. S. Furniture, where each piece whispers a unique story. Founded in [year], we transform spaces into living narratives, crafting artifacts that resonate with the poetry of life.
+                  Welcome to R. S. Furniture, where each piece whispers a unique
+                  story. Founded in [year], we transform spaces into living
+                  narratives, crafting artifacts that resonate with the poetry
+                  of life.
                 </p>
               </div>
               <div className="md:w-[30%] w-[90%] text-white mb-10">
-                <h2 className="md:text-4xl text-2xl md:font-bold font-semibold mb-3 text-blue-300 leading-tight">Elegance, Comfort, and You</h2>
+                <h2 className="md:text-4xl text-2xl md:font-bold font-semibold mb-3 text-blue-300 leading-tight">
+                  Elegance, Comfort, and You
+                </h2>
                 <p className="text-gray-400 leading-loose">
-                  At R. S. Furniture, we go beyond aesthetics; we design comfort, elegance, and a haven for your soul. Each creation is a fusion of timeless design and contemporary living, reflecting your unique style.
+                  At R. S. Furniture, we go beyond aesthetics; we design
+                  comfort, elegance, and a haven for your soul. Each creation is
+                  a fusion of timeless design and contemporary living,
+                  reflecting your unique style.
                 </p>
               </div>
             </section>
@@ -112,17 +181,23 @@ export default function Home() {
             {/* CONTACT Section */}
             <section className="w-full p-5 flex md:flex-row flex-col items-center justify-evenly text-center">
               <div className="md:w-[50%] w-[90%] text-white">
-                <h2 className="md:text-4xl text-2xl md:font-bold font-semibold mb-3 text-teal-400 leading-tight">Get in Touch</h2>
+                <h2 className="md:text-4xl text-2xl md:font-bold font-semibold mb-3 text-teal-400 leading-tight">
+                  Get in Touch
+                </h2>
                 <p className="text-gray-400 leading-loose">
-                  We{"'"}d love to hear from you! Whether you have questions about our products, want to discuss a custom order, or just want to say hello, we{"'"}re here for you.
+                  We{"'"}d love to hear from you! Whether you have questions
+                  about our products, want to discuss a custom order, or just
+                  want to say hello, we{"'"}re here for you.
                 </p>
                 {/* Button to navigate to the /contact page */}
                 <button
                   className="outline outline-2 max-w-[90%] text-base outline-white my-10 rounded-full p-5 font-medium px-10 transition-all hover:outline-[4px] shadow-[0px_30px_60px_rgba(255,255,255,0.3)] inline-flex flex-row items-center  gap-4 hover:gap-5"
-                  onClick={() => router.push('/contact')}   
+                  onClick={() => router.push("/contact")}
                 >
-                    <span className="text-left">Contact Us</span>
-                    <span><AiOutlineArrowRight/></span>
+                  <span className="text-left">Contact Us</span>
+                  <span>
+                    <AiOutlineArrowRight />
+                  </span>
                 </button>
               </div>
             </section>
@@ -138,12 +213,10 @@ export default function Home() {
               </div>
             </button>
 
-
-
             <div className="fixed w-full bottom-0 h-40 fixedTransparentGradient"></div>
           </div>
         </div>
       </div>
     </>
-  )
+  );
 }
--- pages/services.tsx
+++ pages/services.tsx
@@ -1,27 +1,179 @@
 "use client";
-import Image from "next/image"
-import { useRouter } from "next/router"
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { useEffect, useRef } from "react";
+import { IoIosArrowUp } from "react-icons/io";
+import {
+  FaChair,
+  FaPaintBrush,
+  FaBolt,
+  FaBuilding,
+  FaWrench,
+  FaArrowAltCircleUp,
+} from "react-icons/fa";
 
 export default function Services() {
-    const router = useRouter();
+  const router = useRouter();
+  const scrollContainerRef = useRef<HTMLElement>(null);
+
+  useEffect(() => {
+    document.title = "R. S. Furniture";
+  });
+
+  const scrollToTop = () => {
+    if (scrollContainerRef.current) {
+      scrollContainerRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  const services = [
+    { icon: <FaChair />, name: "Furniture" },
+    { icon: <FaPaintBrush />, name: "Painting" },
+    { icon: <FaBolt />, name: "Electric" },
+    { icon: <FaBuilding />, name: "Civil" },
+    { icon: <FaWrench />, name: "Plumbing" },
+    { icon: <FaArrowAltCircleUp />, name: "False Ceiling" },
+  ];
+
   return (
     <>
-      <div className="h-full w-full absolute">
+      <div className="h-full w-full absolute ">
         <div className="md:p-10 p-4 w-full h-full fixed">
-          <div className="border-[1px] shadow-slate-200  border-white w-full h-full rounded-none overflow-y-auto overflow-x-hidden  flex flex-col items-center relative pb-56">
+          <div
+            className="border-[1px] shadow-slate-200  border-white w-full h-full rounded-none overflow-y-auto overflow-x-hidden  flex flex-col items-center relative"
+            id="ScrollContainer"
+          >
+            <span ref={scrollContainerRef}></span>
             <nav className="flex justify-between p-4 md:flex-row flex-col md:w-[80%] w-[90%] mb-12 mt-4">
-            <h1 className="md:text-2xl md:text-[1.5rem] text-3xl mb-5 font-medium logoCss text-center border-y-2 border-y-gray-600 py-1">R S FURNITURE</h1>
-            <div className="flex flow-row md:justify-evenly justify-between items-center md:w-5/12 w-full md:text-xl text-base"> 
-                <button onClick={()=> router.push('/')}>Home</button> |
-                <button className="bn9"><span> Services</span></button> | 
-                <button onClick={()=> router.push('/contact')}>Contact us</button>
+              <h1 className="md:text-2xl md:text-[1.5rem] text-3xl mb-5 font-medium logoCss text-center border-y-2 border-y-gray-600 py-1">
+                R S FURNITURE
+              </h1>
+              <div className="flex flow-row md:justify-evenly justify-between items-center md:w-5/12 w-full md:text-xl text-base">
+                <button onClick={() => router.push("/")}>Home</button> |
+                <button className="bn9">
+                  <span> Services</span>
+                </button>{" "}
+                |
+                <button onClick={() => router.push("/contact")}>
+                  Contact us
+                </button>
               </div>
             </nav>
+
+            {/*
+            OUR SERVICES->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+            FURNITURE
+            PAINTING 
+            ELECTRIC
+            CIVIL
+            PLUMBING
+            FALSE CEILING */}
+
+            <section className="mb-8 w-full flex flex-col items-center">
+              <h2 className="md:text-5xl text-3xl font-semibold mb-10 text-rose-300">
+                Our Services
+              </h2>
+              <div className="md:w-[50%] w-[90%] flex flex-col gap-10">
+                {/* Furniture */}
+                <div className="flex items-center px-7 md:rounded-sm mb-4 rounded-3xl md:-ml-12 flex-row bg-gradient-to-r from-blue-500 to-indigo-700 shadow-[0px_30px_60px_rgba(59,130,246,0.22)] md:h-36 h-48">
+                  <div className="mr-4">
+                    <FaChair className="h-10 w-10 text-white" />
+                  </div>
+                  <div>
+                    <p className="text-2xl text-white">Furniture</p>
+                    <p className="text-white">
+                      Explore our collection of exquisite furniture pieces
+                      designed for both comfort and style.
+                    </p>
+                  </div>
+                </div>
+
+                {/* Painting */}
+                <div className="flex items-center px-7 md:rounded-sm mb-4 rounded-3xl md:-mr-12 flex-row-reverse bg-gradient-to-r from-pink-500 to-purple-700 shadow-[0px_30px_60px_rgba(219,39,119,0.22)] md:h-36 h-48">
+                  <div className="ml-4">
+                    <FaPaintBrush className="h-10 w-10 text-white" />
+                  </div>
+                  <div>
+                    <p className="text-2xl text-white">Painting</p>
+                    <p className="text-white">
+                      Transform your space with our professional painting
+                      services, adding color and vibrancy to your walls.
+                    </p>
+                  </div>
+                </div>
+
+                {/* Civil */}
+                <div className="flex items-center px-7 md:rounded-sm mb-4 rounded-3xl md:-ml-12 flex-row bg-gradient-to-r from-green-500 to-teal-700 shadow-[0px_30px_60px_rgba(75,192,192,0.22)] md:h-36 h-48">
+                  <div className="mr-4">
+                    <FaBuilding className="h-10 w-10 text-white" />
+                  </div>
+                  <div>
+                    <p className="text-2xl text-white">Civil</p>
+                    <p className="text-white">
+                      Benefit from our expertise in civil engineering, ensuring
+                      strong and reliable structures for your projects.
+                    </p>
+                  </div>
+                </div>
+
+                {/* Electric */}
+                <div className="flex items-center px-7 md:rounded-sm mb-4 rounded-3xl md:-mr-12 flex-row-reverse bg-gradient-to-r from-yellow-500 to-orange-700 shadow-[0px_30px_60px_rgba(253,203,110,0.22)] md:h-36 h-48">
+                  <div className="ml-4">
+                    <FaBolt className="h-10 w-10 text-white" />
+                  </div>
+                  <div>
+                    <p className="text-2xl text-white">Electric</p>
+                    <p className="text-white">
+                      Ensure a seamless electrical setup with our electrical
+                      services, providing safety and efficiency.
+                    </p>
+                  </div>
+                </div>
+
+                {/* Plumbing */}
+                <div className="flex items-center px-7 md:rounded-sm mb-4 rounded-3xl md:-ml-12 flex-row bg-gradient-to-r from-red-500 to-pink-700 shadow-[0px_30px_60px_rgba(239,68,68,0.22)] md:h-36 h-48">
+                  <div className="mr-4">
+                    <FaWrench className="h-10 w-10 text-white" />
+                  </div>
+                  <div>
+                    <p className="text-2xl text-white">Plumbing</p>
+                    <p className="text-white">
+                      Experience smooth water flow and drainage systems with our
+                      top-notch plumbing solutions.
+                    </p>
+                  </div>
+                </div>
+
+                {/* False Ceiling */}
+                <div className="flex items-center px-7 md:rounded-sm mb-4 rounded-3xl md:-mr-12 flex-row-reverse bg-gradient-to-r from-gray-500 to-cool-gray-700 shadow-[0px_30px_60px_rgba(169,179,194,0.22)] md:h-36 h-48">
+                  <div className="ml-4">
+                    <FaArrowAltCircleUp className="h-10 w-10 text-white" />
+                  </div>
+                  <div>
+                    <p className="text-2xl text-white">False Ceiling</p>
+                    <p className="text-white">
+                      Elevate your space with our false ceiling solutions,
+                      adding a touch of sophistication and modernity.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </section>
+
+            <button
+              className="w-full mt-5 p-4 shadow-md cursor-pointer transition-all bg-gradient-to-b from-transparent to-transparent hover:bg-gradient-to-b hover:from-transparent hover:to-[#121212] bg-opacity-10 z-30"
+              onClick={scrollToTop}
+            >
+              <div className="flex flex-col items-center ">
+                <IoIosArrowUp className="h-10 w-12" />
+                <p>Back to Top</p>
+              </div>
+            </button>
 
             <div className="fixed w-full bottom-0 h-40 fixedTransparentGradient"></div>
           </div>
         </div>
       </div>
     </>
-  )
+  );
 }
